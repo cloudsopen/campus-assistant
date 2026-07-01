d0e958845b11c949a1bf37180c57c1b01862b699
cloudsopen <m18751935929@163.com>
2026-07-01 12:13:22 +0800

    开发

---
+++ app/src/main/java/com/example/campusassistant/PublishBuyActivity.kt
@@ -0,0 +1,13 @@
+package com.example.campusassistant
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.campusassistant.R
+
+class PublishBuyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 绑定对应的布局文件
+        setContentView(R.layout.activity_publish_buy)
+    }
+}
